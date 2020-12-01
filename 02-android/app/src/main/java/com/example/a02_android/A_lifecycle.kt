f76647d7cfa63d9a49a52330dabc91d0a96cbfc3
Alex Charco <alex.charco@epn.edu.ec>
2020-12-01 12:50:13 -0500

    Android Studio - Clase dic-01 - Ciclo de Vida

---
+++ 02-android/app/src/main/java/com/example/a02_android/A_lifecycle.kt
@@ -0,0 +1,93 @@
+package com.example.a02_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class A_lifecycle : AppCompatActivity() {
+    var total = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a_lifecycle)
+
+        Log.i("ciclo-vida","oncreate")
+
+        val btSum=findViewById<Button>(R.id.btn_ciclo_vida)
+        val txTotal=findViewById<TextView>(R.id.txv_ciclo_vida)
+
+
+        btSum.setOnClickListener{
+            total=total+1
+            txTotal.text=total.toString()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("ciclo-vida","onSaveInstance")
+        if(outState!=null){
+            outState.run {
+                //Aqui es donde se ira a guardar las cosas
+                //Solo se pueden guardar cosas como:
+                //Guardar solo primiticos - int, bool, char, etc
+                //Si queremos guardar clases no se podra por ahora
+                //put -> tipo de dato que queramos
+                putInt("totalGuardado",total)
+                    //Se guarda como: llave-valor
+                    //aunque pareciera que no se guardo nada, si esta almacenando por detras el valor de las variables
+
+
+            }
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i("ciclo-vida","onRestoreInstance")
+        val recoverVal = savedInstanceState.getInt("totalGuardado")
+        if(recoverVal!=null){
+            total=recoverVal
+
+            var txTotal = findViewById<TextView>(R.id.txv_ciclo_vida)
+            txTotal.text=total.toString()
+        }
+    }
+
+
+    override fun onStart(){
+        super.onStart()
+        Log.i("ciclo-vida","onstart")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.i("ciclo-vida","onresume")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.i("ciclo-vida","onpause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.i("ciclo-vida","onstop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i("ciclo-vida","ondestroy")
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        Log.i("ciclo-vida","onstart")
+    }
+
+
+}
--- 02-android/app/src/main/java/com/example/a02_android/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/a02_android/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.a02_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("main1","oncreate")
+
+        val btCicloVida = findViewById<Button>(R.id.button_ir_ciclo_vida)
+        btCicloVida.setOnClickListener{
+            irLifeCycle()
+        }
+
     }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i("main1","ondestroy")
+    }
+
+
+    fun irLifeCycle(){
+        val intentExplicito = Intent(
+            this,
+            A_lifecycle::class.java
+        )
+        startActivity(intentExplicito)
+    }
+
 }
